51b4c6d660f46885a0f8f31a3cfd371b863de126
Miquel Puig <mpuig22@gmail.com>
2019-09-10 18:11:27 +0200

    virtual scroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VirtualComponent } from './components/virtual/virtual.component';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VirtualComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ScrollingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
